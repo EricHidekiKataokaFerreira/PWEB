65787e89245df73f10904653f3ffc3516f11b42d
Eric Hideki <69849503+EricHidekiKataokaFerreira@users.noreply.github.com>
2021-11-07 14:57:05 -0300

    Delete 1.0_PORTIFOLIO directory

--- 1.0_PORTIFOLIO/circularText.js
+++
@@ -1,9 +0,0 @@
-
-const text = document.querySelector('.text');
-text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
-
-const element = document.querySelectorAll('span');
-for(let i = 0; i < element.length; i++){
-    element[i].style.transform = "rotate("+i*20+"deg)";
-}
-
